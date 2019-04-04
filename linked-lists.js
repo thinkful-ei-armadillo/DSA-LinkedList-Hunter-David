6524beb725d45d606e45f83a94aad45f8d8df0ce
Hunter Kreshock <hkreshock@gmail.com>
2019-04-04 09:48:40 -0700

    drills 3, 4

--- linked-lists.js
+++ linked-lists.js
@@ -17,8 +17,7 @@ class LinkedList {
   insertLast(item) {
     if (this.head === null) {
       this.insertFirst(item);
-    }
-    else {
+    } else {
       let tempNode = this.head;
       while (tempNode.next !== null) {
         tempNode = tempNode.next;
@@ -26,30 +25,29 @@ class LinkedList {
       tempNode.next = new _Node(item, null);
     }
   }
-  find(item) { 
-  // Start at the head
+  find(item) {
+    // Start at the head
     let currNode = this.head;
     // If the list is empty
     if (!this.head) {
       return null;
     }
-    // Check for the item 
+    // Check for the item
     while (currNode.value !== item) {
       /* Return null if it's the end of the list 
          and the item is not on the list */
       if (currNode.next === null) {
         return null;
-      }
-      else {
-        // Otherwise, keep looking 
+      } else {
+        // Otherwise, keep looking
         currNode = currNode.next;
       }
     }
     // Found it
     return currNode;
   }
-  remove(item){ 
-  // If the list is empty
+  remove(item) {
+    // If the list is empty
     if (!this.head) {
       return null;
     }
@@ -63,8 +61,8 @@ class LinkedList {
     // Keep track of previous
     let previousNode = this.head;
 
-    while ((currNode !== null) && (currNode.value !== item)) {
-      // Save the previous node 
+    while (currNode !== null && currNode.value !== item) {
+      // Save the previous node
       previousNode = currNode;
       currNode = currNode.next;
     }
@@ -74,44 +72,44 @@ class LinkedList {
     }
     previousNode.next = currNode.next;
   }
-  insertBefore(newValue, beforeTarget){
-    if(!this.head){
+  insertBefore(newValue, beforeTarget) {
+    if (!this.head) {
       this.insertFirst(newValue);
-    } 
+    }
     let currNode = this.head;
     let previousNode = this.head;
 
-    while((currNode !== null) && (currNode.value !== beforeTarget)){
-    // Save the previous node 
+    while (currNode !== null && currNode.value !== beforeTarget) {
+      // Save the previous node
       previousNode = currNode;
       currNode = currNode.next;
     }
-    if (currNode === null){
+    if (currNode === null) {
       console.log('Target not found');
       return;
     }
     previousNode.next = new _Node(newValue, previousNode.next);
   }
-  insertAfter(newValue, afterTarget){
-    if(!this.head){
+  insertAfter(newValue, afterTarget) {
+    if (!this.head) {
       this.insertFirst(newValue);
-    } 
+    }
     let currNode = this.head;
-    while((currNode.next !== null) && (currNode.value !== afterTarget)){
+    while (currNode.next !== null && currNode.value !== afterTarget) {
       currNode = currNode.next;
     }
-    if(currNode.next === null){
+    if (currNode.next === null) {
       console.log('Target not found');
       return;
     }
     let newNode = new _Node(newValue, currNode.next);
     currNode.next = newNode;
   }
-  insertAt(newValue, position){
-    let count = 0; 
+  insertAt(newValue, position) {
+    let count = 1;
     let currNode = this.head;
-    while(count < position){
-      if(currNode.next === null){
+    while (count < position) {
+      if (currNode.next === null) {
         console.log('Could not find that position');
         return;
       }
@@ -122,7 +120,7 @@ class LinkedList {
   }
 }
 
-function main(){
+function main() {
   let SLL = new LinkedList();
   SLL.insertLast('Apollo');
   SLL.insertLast('Boomer');
@@ -135,6 +133,67 @@ function main(){
   SLL.insertAfter('Hotdog', 'Helo');
   SLL.insertAt('Kat', 3);
   SLL.remove('Tauhida');
+  display(SLL);
+  size(SLL);
+  ifExists(SLL);
+  findPrevious(SLL, 'Helo');
+  lastVal(SLL);
 }
 
 main();
+
+function display(linkedList) {
+  let currentNode = linkedList.head;
+  while (currentNode.next !== null) {
+    console.log(currentNode.value);
+    currentNode = currentNode.next;
+  }
+  console.log(currentNode.value);
+}
+
+function size(linkedList) {
+  let currentNode = linkedList.head;
+  let count = 0;
+  while (currentNode.next !== null) {
+    currentNode = currentNode.next;
+    count++;
+  }
+  count++;
+  console.log(count);
+}
+
+function ifExists(linkedList) {
+  if (!linkedList.head) {
+    console.log('no list exists');
+  }
+  console.log('there is a list');
+}
+
+function findPrevious(linkedList, str) {
+  if (!linkedList.head) {
+    console.log('value doesnt exist');
+  }
+  let currNode = linkedList.head;
+  let previousNode = linkedList.head;
+
+  while (currNode !== null && currNode.value !== str) {
+    // Save the previous node
+    previousNode = currNode;
+    currNode = currNode.next;
+  }
+  if (currNode === null) {
+    console.log('Target not found');
+    return;
+  }
+  console.log(previousNode.value);
+}
+
+function lastVal(linkedList) {
+  let currentNode = linkedList.head;
+  while (currentNode.next !== null) {
+    currentNode = currentNode.next;
+  }
+  console.log(currentNode.value);
+}
+
+// WhatDoesThisProgramDo() is a function that replaces duplicates with the next value
